8299d20dada03a3baf821dc26c8d220d091ddad1
aosoft <tan-y@aosoft.jp>
2017-11-18 03:35:09 +0900

    implements CppSample1 (read function table from this pointer directly)

--- CppInvokeSampleApp/Program.cs
+++ CppInvokeSampleApp/Program.cs
@@ -10,6 +10,20 @@ namespace CppInvokeSampleApp
 	{
 		static void Main(string[] args)
 		{
+			using (var cpp = new CppSample1())
+			{
+				for (int i = 0; i < 5; i++)
+				{
+					cpp.Add(10);
+					Console.WriteLine(cpp.GetCurrentValue());
+				}
+				for (int i = 0; i < 5; i++)
+				{
+					cpp.Sub(5);
+					Console.WriteLine(cpp.GetCurrentValue());
+				}
+			}
+
 		}
 	}
 }
